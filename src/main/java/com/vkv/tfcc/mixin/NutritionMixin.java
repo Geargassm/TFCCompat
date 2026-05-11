e75675781cacb03d7c3241afce3efbdcbeef99ef
Geargassm <153346686+Geargassm@users.noreply.github.com>
2026-05-11 14:56:36 -0600

    Replace nutrition overwrite with injection

--- src/main/java/com/vkv/tfcc/mixin/NutritionMixin.java
+++ src/main/java/com/vkv/tfcc/mixin/NutritionMixin.java
@@ -1,12 +1,14 @@
 package com.vkv.tfcc.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(targets = "net.dries007.tfc.common.capabilities.food.NutritionData")
+@Mixin(targets = "net.dries007.tfc.common.capabilities.food.NutritionData", remap = false)
 public class NutritionMixin {
-    @Overwrite(remap = false)
-    public float getAverageNutrition() {
-        return 1.0f;
+    @Inject(method = "getAverageNutrition", at = @At("HEAD"), cancellable = true, remap = false, require = 0)
+    private void tfccompat$getAverageNutrition(CallbackInfoReturnable<Float> cir) {
+        cir.setReturnValue(1.0f);
     }
 }
